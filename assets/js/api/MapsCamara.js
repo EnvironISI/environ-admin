4f57a0b776c0f5d75d8fcf39d871ba5491cb8eb1
Hugo Carvalho <hugocarvalho@ioacademy.iotech.pt>
2020-03-21 00:11:48 +0000

    Maps for Admin Dashboard with all users locations overview

--- assets/js/api/MapsCamara.js
+++ assets/js/api/MapsCamara.js
@@ -1,92 +1,14 @@
 //
 // Google maps
 //
-
-function getInputValue() {
-    var input = document.getElementById('inputLocal').value;
-    document.getElementById('inputLocal').value = "";
-    fetch('https://api.opencagedata.com/geocode/v1/json?q=' + input + '&key=e266ba8c43b346eab28695023463e6ff')
-        .then(function (response) {
-            return response.json();
-        }).then(function (data) {
-            var lat = data.results[0].geometry.lat; // this will be a string
-            var lng = data.results[0].geometry.lng;
-            initMap(lat, lng);
-        })
-        .catch(function () {
-            // catch any errors
-        }); // 8011224 Guimaraes
-
-}
-
-function runScript(e) {
-    //See notes about 'which' and 'key'
-    if (e.keyCode == 13) {
-        getInputValue();
-        return false;
-    }
-}
-
-var $map = $('#map-default'),
-    map,
-    lat,
-    lng,
-    color = "#1d84b5";
-
-function initMap() {
-
-    map = document.getElementById('map-default');
-    lat = map.getAttribute('data-lat');
-    lng = map.getAttribute('data-lng');
-
-    var myLatlng = new google.maps.LatLng(lat, lng);
-    var mapOptions = {
-        zoom: 12,
-        scrollwheel: false,
-        center: myLatlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-    }
-
-    map = new google.maps.Map(map, mapOptions);
-
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        animation: google.maps.Animation.DROP,
-        title: 'Hello World!'
-    });
-
-    var contentString = '<div class="info-window-content"><h2>Argon Dashboard PRO</h2>' +
-        '<p>A beautiful premium dashboard for Bootstrap 4.</p></div>';
-
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
-
-    google.maps.event.addListener(marker, 'click', function () {
-        infowindow.open(map, marker);
-    });
-}
-
-if ($map.length) {
-    google.maps.event.addDomListener(window, 'load', initMap);
-}
-
-//
-// Google maps
-//
 navigator.geolocation.getCurrentPosition(showPosition);
 
 function showPosition(position) {
     var lat = position.coords.latitude;
     var lng = position.coords.longitude;
     initMap(lat, lng)
+    initMapDashboard(lat, lng)
 }
-
-var $map = $('#map-custom'),
-    map,
-    color = "#1d84b5";
-
 
 function initMap(lat, lng) {
 
@@ -101,14 +23,43 @@ function initMap(lat, lng) {
         // styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":color},{"visibility":"on"}]}]
     }
 
+    var cities = ['Fafe', 'Fafe', 'Guimarães', 'Esposende', 'Braga'];
+    var array = [];
+    cities.forEach(element => {
+        fetch('https://api.opencagedata.com/geocode/v1/json?q=' + element + '&key=e266ba8c43b346eab28695023463e6ff')
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            var markers=[];
+            var a = { lat: data.results[0].geometry.lat, lng:data.results[0].geometry.lng}
+            // array.push(a);
+            for (i = 0; i < cities.length; i++) { 
+                var coo = new google.maps.LatLng(a.lat, a.lng)
+                console.log(coo)
+                markers[i] = new google.maps.Marker({
+                position: coo,
+                map: map,
+                draggable: false,
+                animation: google.maps.Animation.DROP,
+                title: 'Utilizador'
+            });
+        }
+        })
+        .catch(function () {
+            // catch any errors
+        }); // 8011224 Guimaraes
+    
+    })
+    console.log(array.length);
+
     map = new google.maps.Map(map, mapOptions);
 
-    var marker = new google.maps.Marker({
+    marker = new google.maps.Marker({
         position: myLatlng,
         map: map,
         draggable: false,
         animation: google.maps.Animation.DROP,
-        title: 'Hello World!'
+        title: 'Utilizador'
     });
 
     var contentString = '<div class="info-window-content"><h2>Argon Dashboard PRO</h2>' +
@@ -130,6 +81,80 @@ function initMap(lat, lng) {
         document.getElementById('long').value = marker.getPosition().lng()
     }
 }
+
+
+function initMapDashboard(lat, lng) {
+
+    map = document.getElementById('map-camaras');
+
+    var myLatlng = new google.maps.LatLng(lat, lng);
+    var mapOptions = {
+        zoom: 11,
+        scrollwheel: false,
+        center: myLatlng,
+        // mapTypeId: 'google.maps.MapTypeId.ROADMAP',
+        // styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":color},{"visibility":"on"}]}]
+    }
+
+    var cities = ['Fafe', 'Fafe', 'Guimarães', 'Esposende', 'Braga'];
+    var array = [];
+    cities.forEach(element => {
+        fetch('https://api.opencagedata.com/geocode/v1/json?q=' + element + '&key=e266ba8c43b346eab28695023463e6ff')
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            var markers=[];
+            var a = { lat: data.results[0].geometry.lat, lng:data.results[0].geometry.lng}
+            // array.push(a);
+            for (i = 0; i < cities.length; i++) { 
+                var coo = new google.maps.LatLng(a.lat, a.lng)
+                console.log(coo)
+                markers[i] = new google.maps.Marker({
+                position: coo,
+                map: map,
+                draggable: false,
+                animation: google.maps.Animation.DROP,
+                title: 'Utilizador'
+            });
+        }
+        })
+        .catch(function () {
+            // catch any errors
+        }); // 8011224 Guimaraes
+    
+    })
+    console.log(array.length);
+
+    map = new google.maps.Map(map, mapOptions);
+
+    marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        draggable: false,
+        animation: google.maps.Animation.DROP,
+        title: 'Utilizador'
+    });
+
+    var contentString = '<div class="info-window-content"><h2>Argon Dashboard PRO</h2>' +
+        '<p>A beautiful premium dashboard for Bootstrap 4.</p></div>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.open(map, marker);
+    });
+    google.maps.event.addListener(marker, 'dragend', function () {
+        getPo();
+    });
+
+    function getPo() {
+        document.getElementById('lati').value = marker.getPosition().lat();
+        document.getElementById('long').value = marker.getPosition().lng()
+    }
+}
+
 
 if ($map.length) {
     google.maps.event.addDomListener(window, 'load', initMap);
