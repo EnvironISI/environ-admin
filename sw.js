57b80873fa24da647048691479bd3344be673c9c
Hugo Carvalho <hugocarvalho@ioacademy.iotech.pt>
2020-05-06 23:10:11 +0100

    tentativa PWA

---
+++ sw.js
@@ -0,0 +1,101 @@
+/*// This is the "Offline page" service worker
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "https://dai-18-19-front-a84182.c9users.io/fallback/index.html";
+
+// Install stage sets up the offline page in the cache and opens a new cache
+self.addEventListener("install", function(event) {
+  console.log("[PWA Builder] Install Event processing");
+
+  event.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      console.log("[PWA Builder] Cached offline page during install");
+
+      if (offlineFallbackPage === "https://dai-18-19-front-a84182.c9users.io/fallback/index.html") {
+        return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
+      }
+
+      return cache.add(offlineFallbackPage);
+    })
+  );
+});
+
+// If any fetch fails, it will show the offline page.
+self.addEventListener("fetch", function(event) {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request).catch(function(error) {
+      // The following validates that the request was for a navigation to a new document
+      if (
+        event.request.destination !== "document" ||
+        event.request.mode !== "navigate"
+      ) {
+        return;
+      }
+
+      console.error("[PWA Builder] Network request Failed. Serving offline page " + error);
+      return caches.open(CACHE).then(function(cache) {
+        return cache.match(offlineFallbackPage);
+      });
+    })
+  );
+});
+
+// This is an event that can be fired from your page to tell the SW to update the offline page
+self.addEventListener("refreshOffline", function() {
+  const offlinePageRequest = new Request(offlineFallbackPage);
+
+  return fetch(offlineFallbackPage).then(function(response) {
+    return caches.open(CACHE).then(function(cache) {
+      console.log("[PWA Builder] Offline page updated from refreshOffline event: " + response.url);
+      return cache.put(offlinePageRequest, response);
+    });
+  });
+});
+*/
+
+var cacheName = 'environ.';
+var filesToCache = [
+
+  './frontoffice/',
+  './index.html',
+  './frontoffice/css/',
+  './frontOffice/images',
+  './frontOffice/js',
+  './frontOffice/js/main.js',
+  './manifest.json',
+  './sw-register.js',
+  './assets/img/brand/favicon.ico',
+  './assets/img/icons/icon-192x192.png'
+//   'https://dai-18-19-front-a84182.c9users.io/manifest.json',
+//   'https://dai-18-19-front-a84182.c9users.io/sw-register.js',
+//   'https://dai-18-19-front-a84182.c9users.io/views/frontOffice/img/logo_pwa.png',
+//   'https://dai-18-19-front-a84182.c9users.io/views/frontOffice/img/favicon.png'
+
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+self.addEventListener('activate', evt => {
+    console.log("Ativado")
+})
