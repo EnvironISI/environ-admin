45e58c285c906de3ed075d9d5a6938141e826d8f
EnvironISI <environisi@gmail.com>
2020-05-23 21:45:22 +0100

    firebase

--- assets/js/api/firebase-messaging.js
+++ assets/js/api/firebase-messaging.js
@@ -1,17 +1,3 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyB8MYslNwT_i3mCs7UhuiU4ZlF8TFmU5P8",
-    authDomain: "isienviron.firebaseapp.com",
-    databaseURL: "https://isienviron.firebaseio.com",
-    projectId: "isienviron",
-    storageBucket: "isienviron.appspot.com",
-    messagingSenderId: "636194129852",
-    appId: "1:636194129852:web:52d385c18b8140a59fcdb9",
-    measurementId: "G-7X3BC6HX5X"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
 const messaging = firebase.messaging();
 
 messaging.onTokenRefresh(() => {
